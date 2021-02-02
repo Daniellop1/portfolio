546ca96d0673cd0fad2609010cc50bcc0cb4af4d
Daniel <2005danielus@gmail.com>
2021-02-02 22:36:33 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,15 +6,11 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Daniel Lopez`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      // See the theme's README for all available options
       options: {},
     },
     {
